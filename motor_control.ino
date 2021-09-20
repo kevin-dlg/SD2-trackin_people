a1eb74e4927cb1254e7e1dd22f89753bf74a6d55
kevin-dlg <54332161+kevin-dlg@users.noreply.github.com>
2021-09-20 15:24:50 -0500

    Update motor_control.ino
    Bluetooth connection done
    Servo Control done
    We need stepper motor control 
    Need to know:
    type of stepper motor and speed control

--- motor_control.ino
+++ motor_control.ino
@@ -1,4 +1,74 @@
 //arduino code for motor control for the robot
-//requirements: control motor by the press of a key 
-//
-hello my name is esmer
+//requirements: control motor by the press of a key #include <SoftwareSerial.h>
+#include<Servo.h>
+SoftwareSerial HM10(2, 3); // RX = 2, TX = 3
+Servo guide;
+char appData;  
+String inData = "";
+int pos = 90;
+
+void setup() {
+  // put your setup code here, to run once:
+  guide.attach(9);
+  guide.write(pos);
+  Serial.begin(9600);
+  Serial.println("HM10 serial started at 9600");
+  HM10.begin(9600); // set HM10 serial at 9600 baud rate
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  HM10.listen();  // listen the HM10 port
+  while (HM10.available() > 0) {   // if HM10 sends something then read
+    appData = HM10.read();
+    inData = String(appData);  // save the data in string format
+    Serial.write(appData);
+  }
+  if (Serial.available()) 
+  {           // Read user input if available.
+    delay(10);
+    HM10.write(Serial.read());
+  }
+  if ( inData == "W") 
+  {
+    Serial.println("Moving Forward");
+  }
+  else if ( inData == "S") 
+  {
+    Serial.println("Moving Back");
+  }
+  else if ( inData == "D") 
+  {
+    Serial.println("Moving Left");
+    if(pos > 0 && pos < 180){  
+    pos = pos + 5;
+    guide.write(pos);
+    delay(500);
+    }
+    else{
+      Serial.println("out of bounds");
+    }
+  }
+  else if ( inData == "A") 
+  {
+    Serial.println("Moving Right");
+    if(pos > 0 && pos < 180)
+    {  
+      pos = pos - 5;
+      guide.write(pos);
+      delay(500);
+    }
+    else
+    {
+      Serial.println("out of bounds");
+    }
+  }
+    else
+    {
+      Serial.println("cammand not accepted");
+    }
+  }
+}
+  
